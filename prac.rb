d14cf973ec5a14a6fc49b56c013cd692aa555704
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-23 23:00:26 +0000

    Automatically backed up by Learn

--- prac.rb
+++ prac.rb
@@ -41,7 +41,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|season, holiday|
     puts "#{season.capitalize}:"
     holiday.each {|holiday, supplies|
-      holi_array = holiday.split("_")
+      holi_array = holiday.to_s
+      .split("_")
       puts holi_array
       holi_string = holi_array.capitalize.join(" ")
       string = supplies.join(", ")
