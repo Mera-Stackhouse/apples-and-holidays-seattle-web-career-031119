745eedfda4069a54bc8406172b50524b7a072cd6
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-23 21:59:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,8 +62,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  array = []
   holiday_hash.each {|key, value|
-    if value.include? 
+    if value.include?("BBQ")
+      array << key
   }
 end
 
