06f778c79c1645374d967ae544961f8b465bd66b
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-23 22:51:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season}:"
     holiday.each {|holiday, supplies|
       string = supplies.join(", ")
-      puts "#{holiday}: #{string}"
+      puts "\t#{holiday}: #{string}"
     }
   }
 
